c7472c630ba7f59443455234facf9440348564c2
Alexandru F. Iosif-Lazar <afla@itu.dk>
2016-10-19 08:04:58 +0200

    Updated paths in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-JAVA_DEV_ROOT=~/xtc
-CLASSPATH=~/xtc/classes:~/xtc/bin/junit.jar:~/xtc/bin/javabdd.jar
+JAVA_DEV_ROOT=~/repos/models-team-xtc
+CLASSPATH=~/repos/models-team-xtc/classes:~/repos/models-team-xtc/bin/junit.jar:~/repos/models-team-xtc/bin/javabdd.jar:~/repos/models-team-xtc/bin/org.sat4j.core.jar
 JAVA_HOME=/usr/bin
 PATH_SEP=:
 export JAVA_DEV_ROOT CLASSPATH JAVA_HOME PATH_SEP
