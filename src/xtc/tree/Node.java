91444ad7895799dd96f49b0e72123856d8863df8
Alexandru F. Iosif-Lazar <afla@itu.dk>
2016-11-02 15:57:51 +0100

    Node indexing fixed

--- src/xtc/tree/Node.java
+++ src/xtc/tree/Node.java
@@ -529,9 +529,18 @@ public abstract class Node implements Iterable<Object>, Locatable {
     final int size = size();
     for (int i=0; i<size; i++) {
       Object child = get(i);
-      if (null == o ? null == child : o.equals(child)) return i;
+      if (null == o ? null == child : o == child) return i;
     }
     return -1;
+  }
+  
+  public int firstIndexOf(Object o) {
+	 final int size = size();
+	 for (int i=0; i<size; i++) {
+	   Object child = get(i);
+	   if (null == o ? null == child : o.equals(child)) return i;
+	 }
+	 return -1;
   }
 
   /**
